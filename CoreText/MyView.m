c5bfcc0ce417b5c48a69ebd0ca26a8c49dc3818c
yifanfenfei <jianfeng224@gmail.com>
2014-11-26 20:32:35 +0800

    CoreText pratice
    learn about CoreText , familiar with CTFrame CTLineRef….

--- CoreText/MyView.m
+++ CoreText/MyView.m
@@ -101,6 +101,7 @@
     CFRelease(frame);
 }
 */
+/*
 -(void)drawRect:(CGRect)rect{
 
     NSString * str=[NSString stringWithFormat:@"sunasdfasdf孙啊济南发生地方"];
@@ -156,6 +157,293 @@
     CGContextStrokePath(context);
     
     
+}
+*/
+/*
+// 转换当前的text matrix 就可以正确绘画
+-(void)drawRect:(CGRect)rect{
+    NSString *src = @"其实流程是这样的： 1、生成要绘制的NSAttributedString对象。 ";
+    
+    NSMutableAttributedString * mabstring = [[NSMutableAttributedString alloc]initWithString:src];
+    
+    long slen = [mabstring length];
+    
+    
+    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)mabstring);
+    
+    CGMutablePathRef Path = CGPathCreateMutable();
+    
+    //坐标点在左下角
+    CGPathAddRect(Path, NULL ,CGRectMake(10 , 10 ,self.bounds.size.width-20 , self.bounds.size.height-20));
+    
+    CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, 0), Path, NULL);
+    
+    
+    //得到frame中的行数组
+    CFArrayRef rows = CTFrameGetLines(frame);
+    
+    if (rows) {
+        const CFIndex numberOfLines = CFArrayGetCount(rows);
+        const CGFloat fontLineHeight = [UIFont systemFontOfSize:20].lineHeight;
+        CGFloat textOffset = 0;
+        
+        CGContextRef ctx = UIGraphicsGetCurrentContext();
+        CGContextSaveGState(ctx);
+        NSLog(@"rect =%@",NSStringFromCGRect(rect));
+        // 这次只调整当前的text matrix 就可以
+ 
+        CGContextTranslateCTM(ctx, rect.origin.x, rect.origin.y+[UIFont systemFontOfSize:20].ascender);
+        // saveContext 对当前的text matrix没有效果
+        CGContextSetTextMatrix(ctx, CGAffineTransformMakeScale(1,-1));
+ 
+        
+        
+        for (CFIndex lineNumber=0; lineNumber<numberOfLines; lineNumber++) {
+            CTLineRef line = CFArrayGetValueAtIndex(rows, lineNumber);
+            float flush;
+            switch (0) {
+                case NSTextAlignmentCenter: flush = 0.5;    break; //1
+                case NSTextAlignmentRight:  flush = 1;      break; //2
+                case NSTextAlignmentLeft:  //0
+                default:                    flush = 0;      break;
+            }
+            
+            CFIndex index= CTLineGetStringIndexForPosition( line, CGPointMake(10, 0));
+            NSLog(@"index= %li",index);
+            
+            CGFloat penOffset = CTLineGetPenOffsetForFlush(line, flush, rect.size.width);
+            NSLog(@"penOffset = %f",penOffset);
+            CGContextSetTextPosition(ctx, penOffset, textOffset);//在偏移量x,y上打印
+            CTLineDraw(line, ctx);//draw 行文字
+            textOffset += fontLineHeight;
+        }
+        
+        CGContextRestoreGState(ctx);
+        
+    }
+}
+*/
+/*
+ // 使用CTLineRef 获取行高等信息
+-(void)drawRect:(CGRect)rect
+{
+    NSString *src = @"其实流程是这样的： 1、生成要绘制的NSAttributedString对象。 ";
+    
+    NSMutableAttributedString * mabstring = [[NSMutableAttributedString alloc]initWithString:src];
+    
+    NSMutableDictionary* attriDic=[[NSMutableDictionary alloc]initWithCapacity:0];
+    [attriDic setObject:(id)[UIColor blueColor].CGColor forKey:(id)kCTForegroundColorAttributeName];
+    UIFont * font_=[UIFont systemFontOfSize:20];
+    CTFontRef font=CTFontCreateWithName((CFStringRef)font_.fontName, 20, NULL);
+    [attriDic setObject:(__bridge id)font forKey:(id)kCTFontAttributeName];
+    
+    NSLog(@"font lineheight =%f",font_.lineHeight);// 行高
+    // begin 不准确
+    NSLog(@"font ascending =%f",font_.ascender);
+    NSLog(@"desc =%f",font_.descender);
+    NSLog(@"leading =%f",font_.leading);
+    NSLog(@"capHeight =%f",font_.capHeight);
+    NSLog(@"xHeight =%f",font_.xHeight);
+    // end
+    // leading+ascender+|descender|=lineHeight
+    [mabstring addAttributes:attriDic range: NSMakeRange(0, mabstring.length)];
+    
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    
+    CGContextSetTextMatrix(ctx , CGAffineTransformIdentity);
+    
+    //CGContextSaveGState(ctx);
+    
+    //x，y轴方向移动
+    CGContextTranslateCTM(ctx , 0 ,self.bounds.size.height);
+    
+    //缩放x，y轴方向缩放，－1.0为反向1.0倍,坐标系转换,沿x轴翻转180度
+    CGContextScaleCTM(ctx, 1.0 ,-1.0);
+    
+    // layout master
+    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString(
+                                                                           (CFAttributedStringRef)mabstring);
+    CGMutablePathRef Path = CGPathCreateMutable();
+    
+    //坐标点在左下角
+    CGPathAddRect(Path, NULL ,CGRectMake(0 , 0 ,self.bounds.size.width , self.bounds.size.height));
+    
+    CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, 0), Path, NULL);
+    
+    CFArrayRef Lines = CTFrameGetLines(frame);
+    
+    CFIndex linecount = CFArrayGetCount(Lines);
+    
+    CGPoint origins[linecount];
+    CTFrameGetLineOrigins(frame,
+                          CFRangeMake(0, 0), origins);
+    NSInteger lineIndex = 0;
+    
+    for (id oneLine in (__bridge NSArray *)Lines)
+    {
+        // bounds 觉得不包含行间距
+        CGRect lineBounds = CTLineGetImageBounds((CTLineRef)oneLine, ctx);
+        NSLog(@"lineBounds =%@",NSStringFromCGRect(lineBounds));
+        CGFloat asc,des,lead;
+        double lineWidth = CTLineGetTypographicBounds((CTLineRef)oneLine, &asc, &des, &lead);
+        // 计算准确
+        NSLog(@"ascent = %f,descent = %f,leading = %f,lineWidth = %f",asc,des,lead,lineWidth);
+        lineBounds.origin.x += origins[lineIndex].x;
+        lineBounds.origin.y += origins[lineIndex].y;
+        
+        lineIndex++;
+        //画长方形
+        
+        //设置颜色，仅填充4条边
+        CGContextSetStrokeColorWithColor(ctx, [[UIColor redColor] CGColor]);
+        //设置线宽为1
+        CGContextSetLineWidth(ctx, 1.0);
+        //设置长方形4个顶点
+        CGPoint poins[] = {CGPointMake(lineBounds.origin.x, lineBounds.origin.y),CGPointMake(lineBounds.origin.x+lineBounds.size.width, lineBounds.origin.y),CGPointMake(lineBounds.origin.x+lineBounds.size.width, lineBounds.origin.y+lineBounds.size.height),CGPointMake(lineBounds.origin.x, lineBounds.origin.y+lineBounds.size.height)};
+        CGContextAddLines(ctx,poins,4);
+        CGContextClosePath(ctx);
+        CGContextStrokePath(ctx);
+        
+    }
+    
+    
+    
+    CTFrameDraw(frame,ctx);
+    
+    CGPathRelease(Path);
+    CFRelease(framesetter);
+}
+ */
+void RunDelegateDeallocCallback( void* refCon ){
+    
+}
+
+CGFloat RunDelegateGetAscentCallback( void *refCon ){
+    NSString *imageName = (__bridge NSString *)refCon;
+    return [UIImage imageNamed:imageName].size.height;
+}
+
+CGFloat RunDelegateGetDescentCallback(void *refCon){
+    return 120;
+}
+
+CGFloat RunDelegateGetWidthCallback(void *refCon){
+    NSString *imageName = (__bridge NSString *)refCon;
+    return [UIImage imageNamed:imageName].size.width;
+}
+-(void)drawRect:(CGRect)rect{
+    CGContextRef context=UIGraphicsGetCurrentContext();
+//    CGContextSetTextMatrix(context, CGAffineTransformIdentity);
+//    CGContextTranslateCTM(context, 0, rect.size.height);
+//    CGContextScaleCTM(context, 1.0, -1.0);
+    CGContextSetTextMatrix(context, CGAffineTransformIdentity);//设置字形变换矩阵为CGAffineTransformIdentity，也就是说每一个字形都不做图形变换
+    
+    CGAffineTransform flipVertical = CGAffineTransformMake(1,0,0,-1,0,self.bounds.size.height);
+    CGContextConcatCTM(context, flipVertical);//将当前context的坐标系进行flip
+    NSLog(@"bh=%f",self.bounds.size.height);
+    
+    NSMutableAttributedString * string=[[NSMutableAttributedString alloc]initWithString:@" 插入一张图片的xcga sdfad方法真多发点啊的事发生的发生打发似的阿萨德发阿萨德发是短发撒旦发射点法阿萨德发阿萨德发阿萨德发送到发射点法阿萨德发阿萨德发是短发撒旦发射发射点法是短发 阿萨德发阿萨德发"];
+    
+    
+     /*******************************/
+    CTRunDelegateCallbacks imagesCallBacks;
+    imagesCallBacks.version=kCTRunDelegateCurrentVersion;
+    imagesCallBacks.getWidth=RunDelegateGetWidthCallback;
+    imagesCallBacks.getDescent=RunDelegateGetDescentCallback;
+    imagesCallBacks.getAscent=RunDelegateGetAscentCallback;
+    imagesCallBacks.dealloc=RunDelegateDeallocCallback;
+    
+    NSString * imageName=@"imageName.png";
+    CTRunDelegateRef runDelegate=CTRunDelegateCreate(&imagesCallBacks, (__bridge void *)(imageName));
+    NSMutableAttributedString *imageAttributedString = [[NSMutableAttributedString alloc] initWithString:@" "];//空格用于给图片留位置
+    
+    [imageAttributedString addAttribute:(id)kCTRunDelegateAttributeName value:(__bridge id)runDelegate range:NSMakeRange(0, 1)];
+    CFRelease(runDelegate);
+    
+    // 自定义一个attribute 表示image的占位符
+    [imageAttributedString addAttribute:@"imageName" value:imageName range:NSMakeRange(0, 1)];
+    
+    [string insertAttributedString:imageAttributedString atIndex:4];
+      /*******************************/
+    
+    
+    
+    
+    CTParagraphStyleSetting lineBreakMode;
+    CTLineBreakMode lineBreak = kCTLineBreakByCharWrapping;
+    lineBreakMode.spec = kCTParagraphStyleSpecifierLineBreakMode;
+    lineBreakMode.value = &lineBreak;
+    lineBreakMode.valueSize = sizeof(CTLineBreakMode);
+    
+    CTParagraphStyleSetting settings[] = {
+        lineBreakMode
+    };
+    
+    CTParagraphStyleRef style = CTParagraphStyleCreate(settings, 1);
+    
+    
+    // build attributes
+    NSMutableDictionary *attributes = [NSMutableDictionary dictionaryWithObject:(__bridge id  )style forKey:(id)kCTParagraphStyleAttributeName ];
+    
+    // set attributes to attributed string
+    [string addAttributes:attributes range:NSMakeRange(0, [string length])];
+    
+    
+    
+    CTFramesetterRef ctFramesetter = CTFramesetterCreateWithAttributedString((CFMutableAttributedStringRef)string);
+    
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGRect bounds = CGRectMake(0.0, 0.0, self.bounds.size.width, self.bounds.size.height);
+    CGPathAddRect(path, NULL, bounds);
+    
+    CTFrameRef ctFrame = CTFramesetterCreateFrame(ctFramesetter,CFRangeMake(0, 0), path, NULL);
+    CTFrameDraw(ctFrame, context);
+    // get rows from frame
+    CFArrayRef lines = CTFrameGetLines(ctFrame);
+    CGPoint lineOrigins[CFArrayGetCount(lines)];
+    CTFrameGetLineOrigins(ctFrame, CFRangeMake(0, 0), lineOrigins);
+    
+    NSLog(@"line count = %ld",CFArrayGetCount(lines));
+    for (int i = 0; i < CFArrayGetCount(lines); i++) {
+        CTLineRef line = CFArrayGetValueAtIndex(lines, i);
+        CGFloat lineAscent;
+        CGFloat lineDescent;
+        CGFloat lineLeading;
+        CTLineGetTypographicBounds(line, &lineAscent, &lineDescent, &lineLeading);
+        NSLog(@"ascent = %f,descent = %f,leading = %f",lineAscent,lineDescent,lineLeading);
+        
+        CFArrayRef runs = CTLineGetGlyphRuns(line);
+        NSLog(@"run count = %ld",CFArrayGetCount(runs));
+        for (int j = 0; j < CFArrayGetCount(runs); j++) {
+            CGFloat runAscent;
+            CGFloat runDescent;
+            CGPoint lineOrigin = lineOrigins[i];
+            CTRunRef run = CFArrayGetValueAtIndex(runs, j);
+            NSDictionary* attributes = (NSDictionary*)CTRunGetAttributes(run);
+            CGRect runRect;
+            runRect.size.width = CTRunGetTypographicBounds(run, CFRangeMake(0,0), &runAscent, &runDescent, NULL);
+            NSLog(@"width = %f",runRect.size.width);
+            
+            runRect=CGRectMake(lineOrigin.x + CTLineGetOffsetForStringIndex(line, CTRunGetStringRange(run).location, NULL), lineOrigin.y - runDescent, runRect.size.width, runAscent + runDescent);
+            
+            NSString *imageName = [attributes objectForKey:@"imageName"];
+            //图片渲染逻辑
+            if (imageName) {
+                UIImage *image = [UIImage imageNamed:imageName];
+                if (image) {
+                    CGRect imageDrawRect;
+                    imageDrawRect.size = image.size;
+                    imageDrawRect.origin.x = runRect.origin.x + lineOrigin.x;
+                    imageDrawRect.origin.y = lineOrigin.y;
+                    CGContextDrawImage(context, imageDrawRect, image.CGImage);
+                }
+            }
+        }
+    }
+    
+    CFRelease(ctFrame);
+    CFRelease(path);
+    CFRelease(ctFramesetter);
 }
 
 @end
